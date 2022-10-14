a7b51ca08825c2d1315cef8c77cf68e0d3f52844
GDSCTINT <115734586+GDSCTINT@users.noreply.github.com>
2022-10-14 11:44:17 +0530

    Add files via upload

---
+++ palindrome.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class palindrome
+{
+    public void chkpal(int n)
+    {
+        int p,s=0,m=n;
+        while(m>0)
+        {
+            p=m%10;
+            s=s*10+p;
+            m=m/10;
+        }
+        if(s==n)
+        System.out.print("Palindrome Number");
+        else
+        System.out.print("Palindrome Number");
+        
+    }
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a Number:");
+        int n= in.nextInt();
+        palindrome ob = new palindrome();
+        ob.chkpal(n);
+    }
+} 
